61f7b372b565cae17c3a1aa161cc98c57873db73
Blackbackroom <saranchukvm87@gmail.com>
2023-02-27 18:12:56 +0100

    create tiles

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,7 +3,20 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-
+            <div className="AppBody">
+                <div className="AppBodyLeftColumn">
+                    <div className="Tile ContactInformationTile"></div>
+                    <div className="Tile SkillsTile"></div>
+                </div>
+                <div className="AppBodyRightColumn">
+                    <div className="Tile"></div>
+                    <div className="Tile ExperienceTile"></div>
+                    <div className="Tile"></div>
+                </div>
+            </div>
+            <div className="AppFooter">
+                <div className="Tile"></div>
+            </div>
         </div>
     );
 }
